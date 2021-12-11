69cd28d37e5e7b932f1c338fc05c9486b0d5a7ad
Vaceslav Ignatuk <vaceslavignatuk@gmail.com>
2021-12-12 00:02:37 +0300

    refactoring

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 'use script';
 
-const INPUT = document.querySelector('.input');
-const BUTTON = document.querySelector('.button');
-const OUTPUT_GENDER = document.querySelector('.output-gender');
-const OUTPUT_COUNTRY = document.querySelector('.output-country');
+import { UI_ELEMENTS } from './view.js';
 
 
 function send() {
-	let firstName = INPUT.value;
+	let firstName = UI_ELEMENTS.INPUT.value;
 	const serverUrlGender = 'https://api.genderize.io';
 	const serverUrlCountry = 'https://api.nationalize.io';
 	const urlGender = `${serverUrlGender}?name=${firstName}`;
 	const urlCountry = `${serverUrlCountry}?name=${firstName}`;
+	const isValid = UI_ELEMENTS.INPUT.value === '' || !/^[a-zA-Z]/.test(UI_ELEMENTS.INPUT.value)
 	fetch(urlGender).then(response => response.json()).then(response => {
-		INPUT.value === '' ? OUTPUT_GENDER.textContent = '' : OUTPUT_GENDER.textContent = `${firstName} is ${response["gender"]}`
+		isValid ? UI_ELEMENTS.OUTPUT_GENDER.textContent = '' : UI_ELEMENTS.OUTPUT_GENDER.textContent = `${firstName} is ${response["gender"]}`
 	})
 	fetch(urlCountry).then(response => response.json()).then(response => {
-		INPUT.value === '' ? OUTPUT_COUNTRY.textContent = '' : OUTPUT_COUNTRY.textContent = `${firstName} from ${response["country"][0]["country_id"]}`
+		isValid ? UI_ELEMENTS.OUTPUT_COUNTRY.textContent = '' : UI_ELEMENTS.OUTPUT_COUNTRY.textContent = `${firstName} from ${response["country"][0]["country_id"]}`
 	})
 }
 
-BUTTON.addEventListener('click', send)
+UI_ELEMENTS.BUTTON.addEventListener('click', send)
+
---
+++ view.js
@@ -0,0 +1,8 @@
+'use strict';
+
+export const UI_ELEMENTS = {
+	INPUT: document.querySelector('.input'),
+	BUTTON: document.querySelector('.button'),
+	OUTPUT_GENDER: document.querySelector('.output-gender'),
+	OUTPUT_COUNTRY: document.querySelector('.output-country'),
+}
